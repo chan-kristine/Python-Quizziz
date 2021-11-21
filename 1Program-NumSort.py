11f087fd8db7e9839a351c9c6606b12d641092e2
unknown <chankristineagcay@gmail.com>
2021-11-21 10:47:34 +0800

    Sorts all numbers from higest to lowest and change some variables for better understanding

--- 1Program-NumSort.py
+++ 1Program-NumSort.py
@@ -2,17 +2,109 @@
 # Create a program that ask 4 numbers. 
 # Print the 4 numbers from highest to lowest using only if-else statement.
 
-#step1 - Create a program that ask 4 numbers. 
-num_1= (float(input("Enter your FIRST number here: ")))
-num_2= (float(input("Enter your SECOND number here: ")))
-num_3= (float(input("Enter your THIRD number here: ")))
-num_4= (float(input("Enter your FOURTH number here: " )))
 
-#step2 - Sort the numbers from highest to lowest using only if-else statement.
-def highest_num (Qnty_1, Qnty_2, Qnty_3, Qnty_4):
+
+
+# Create a program that ask 4 numbers. 
+# Create a program that ask 4 numbers. 
+num1 = float(input("\nEnter the FIRST number here:"))
+num2 = float(input("Enter the SECOND (2ND) number here: "))
+num3 = float(input("Enter the THIRD (3RD) number here:  "))
+num4 = float(input("Enter the FOURTH (4TH) number here: "))
+
+
+# Sort the numbers from highest to lowest using only if-else statement.
+def highest_num(Qnty_1, Qnty_2, Qnty_3, Qnty_4):
+
     if Qnty_1 >= Qnty_2 and Qnty_1 >= Qnty_3 and Qnty_1 >= Qnty_4:
         if Qnty_2 >= Qnty_3 and Qnty_2 >= Qnty_4:
             if Qnty_3 >= Qnty_4:
-                number_order= [Qnty_1, Qnty_2, Qnty_3, Qnty_4]
-                print(f" The following set of numbers are in highest to lowest order. ")
-                
+                number_order = [Qnty_1, Qnty_2, Qnty_3, Qnty_4]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_4 >= Qnty_3:
+                number_order = [Qnty_1, Qnty_2, Qnty_4, Qnty_3]
+                print(f"The following numbers is in descending order {number_order}")
+        elif Qnty_3 >= Qnty_2 and Qnty_3 >= Qnty_4:
+            if Qnty_2 >= Qnty_4:
+                number_order = [Qnty_1, Qnty_3, Qnty_2, Qnty_4]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_4 >= Qnty_2:
+                number_order = [Qnty_1, Qnty_3, Qnty_4, Qnty_2]
+                print(f"The following numbers is in descending order {number_order}")
+        elif Qnty_4 >= Qnty_2 and Qnty_4 >= Qnty_3:
+            if Qnty_3 >= Qnty_2:
+                number_order = [Qnty_1, Qnty_4, Qnty_3, Qnty_2]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_2 >= Qnty_3:
+                number_order = [Qnty_1, Qnty_4, Qnty_2, Qnty_3]
+                print(f"The following numbers is in descending order {number_order}")
+    elif Qnty_2 >= Qnty_1 and Qnty_2 >= Qnty_3 and Qnty_2 >= Qnty_4:
+        if Qnty_1 >= Qnty_3 and Qnty_1 >= Qnty_4:
+            if Qnty_3 >= Qnty_4:
+                number_order = [Qnty_2, Qnty_1, Qnty_3, Qnty_4]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_4 >= Qnty_3:
+                number_order = [Qnty_2, Qnty_1, Qnty_4, Qnty_3]
+                print(f"The following numbers is in descending order {number_order}")
+        elif Qnty_3 >= Qnty_1 and Qnty_3 >= Qnty_4:
+            if Qnty_1 >= Qnty_4:
+                number_order = [Qnty_2, Qnty_3, Qnty_1, Qnty_4]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_4 >= Qnty_1:
+                number_order = [Qnty_2, Qnty_3, Qnty_4, Qnty_1]
+                print(f"The following numbers is in descending order {number_order}")
+        elif Qnty_4 >= Qnty_1 and Qnty_4 >= Qnty_3:
+            if Qnty_1 >= Qnty_3:
+                number_order = [Qnty_2, Qnty_4, Qnty_1, Qnty_3]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_3 >= Qnty_1:
+                number_order = [Qnty_2, Qnty_4, Qnty_3, Qnty_1]
+                print(f"The following numbers is in descending order {number_order}")
+    elif Qnty_3 >= Qnty_1 and Qnty_3 >= Qnty_2 and Qnty_3 >= Qnty_4:
+        if Qnty_1 >= Qnty_2 and Qnty_1 >= Qnty_4:
+            if Qnty_2 >= Qnty_4:
+                number_order = [Qnty_3, Qnty_1, Qnty_2, Qnty_4]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_4 >= Qnty_2:
+                number_order = [Qnty_3, Qnty_1, Qnty_4, Qnty_2]
+                print(f"The following numbers is in descending order {number_order}")
+        elif Qnty_2 >= Qnty_1 and Qnty_2 >= Qnty_4:
+            if Qnty_1 >= Qnty_4:
+                number_order = [Qnty_3, Qnty_2, Qnty_1, Qnty_4]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_4 >= Qnty_1:
+                number_order = [Qnty_3, Qnty_2, Qnty_4, Qnty_1]
+                print(f"The following numbers is in descending order {number_order}")
+        elif Qnty_4 >= Qnty_1 and Qnty_4 >= Qnty_2:
+            if Qnty_1 >= Qnty_2:
+                number_order = [Qnty_3, Qnty_4,Qnty_1, Qnty_2]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_2 >= Qnty_1:
+                number_order = [Qnty_3, Qnty_4, Qnty_2, Qnty_1]
+                print(f"The following numbers is in descending order {number_order}")
+    elif Qnty_4 >= Qnty_1 and Qnty_4 >= Qnty_2 and Qnty_4 >= Qnty_3:
+        if Qnty_1 >= Qnty_2 and Qnty_1 >= Qnty_3:
+            if Qnty_2 >= Qnty_3:
+                number_order = [Qnty_4, Qnty_1, Qnty_2, Qnty_3]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_3 >= Qnty_2:
+                number_order = [Qnty_4, Qnty_1, Qnty_3, Qnty_2]
+                print(f"The following numbers is in descending order {number_order}")
+        elif Qnty_2 >= Qnty_1 and Qnty_2 >= Qnty_3:
+            if Qnty_1 >= Qnty_3:
+                number_order = [Qnty_4, Qnty_2, Qnty_1, Qnty_3]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_3 >= Qnty_1:
+                number_order = [Qnty_4, Qnty_2, Qnty_3, Qnty_1]
+                print(f"The following numbers is in descending order {number_order}")
+        elif Qnty_3 >= Qnty_1 and Qnty_3 >= Qnty_2:
+            if Qnty_1 >= Qnty_2:
+                number_order = [Qnty_4, Qnty_3, Qnty_1, Qnty_2]
+                print(f"The following numbers is in descending order {number_order}")
+            elif Qnty_2 >= Qnty_1:
+                number_order = [Qnty_4, Qnty_3, Qnty_2, Qnty_1]
+                print(f"The following numbers is in descending order {number_order}")
+
+highest_num(num1, num2, num3, num4)
+
+    
